87f50e64665fbd58823bf22eb0eccc1616c03fac
Ferdy Herdianto <ferdyherdianto949@gmail.com>
2023-07-31 15:44:07 +0700

    tambah paragraf pada tampilan index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,9 @@
 
 <body>
     <h1>Halo Media Com Binjai</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ab quasi voluptate praesentium asperiores, ducimus
+        distinctio officia iusto ratione eligendi odio repellendus incidunt facere repudiandae eaque, expedita facilis
+        sequi dolore?</p>
 </body>
 
 </html>
