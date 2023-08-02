1a361859cb9b1ee09d8e3ca71265c0a2c16d5de0
Ferdy Herdianto <ferdyherdianto949@gmail.com>
2023-08-02 15:58:20 +0700

    membuat template

---
+++ resources/views/template/navbar.blade.php
@@ -0,0 +1,45 @@
+<header>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('img/img logo/MEDIACOM.png') }}" alt="Media Com Binjai" class="logo-mediacom-header">
+            </a>
+            <button class="navbar-toggler border-0 outline-0" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="fs-1 text-warning"><i class="bi bi-list"></i></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item ms-md-3">
+                        <a class="nav-link text-white fw-semibold active" aria-current="page" href="#">Beranda</a>
+                    </li>
+                    <li class="nav-item dropdown ms-md-3">
+                        <a class="nav-link dropdown-toggle text-white fw-semibold" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Profil
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="#">Tentang Media Com</a></li>
+                            <li><a class="dropdown-item" href="#">Staff dan Instruktur</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item ms-md-3">
+                        <a class="nav-link text-white fw-semibold" href="#">Artikel</a>
+                    </li>
+                    <li class="nav-item dropdown ms-md-3">
+                        <a class="nav-link dropdown-toggle text-white fw-semibold" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Kelas
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="#">Reguler</a></li>
+                            <li><a class="dropdown-item" href="#">Profesi 1 Tahun</a></li>
+                            <li><a class="dropdown-item" href="#">Prakerin / PKL</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
