ef2ab0b0e4b00d230918f299128ea8179ac93aa2
muhamad khadafi <dafi3225@gmail.com>
2023-08-08 23:27:13 +0700

    membuat controller users dan jabatan

--- resources/views/dashboard/layouts/menu.blade.php
+++ resources/views/dashboard/layouts/menu.blade.php
@@ -37,7 +37,7 @@
                     <ul class="nav nav-treeview">
                        
                         <li class="nav-item">
-                            <a href="" class="nav-link">
+                            <a href="/dashboard/user" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Users</p>
                             </a>
