fe67f77cb62f39d17f70594802b1586859d1aa89
Ferdy Herdianto <ferdyherdianto949@gmail.com>
2023-08-11 11:18:02 +0700

    menambahkan detail paket pelatihan pada kelas reguler

--- resources/views/kelas/reguler.blade.php
+++ resources/views/kelas/reguler.blade.php
@@ -1,5 +1,6 @@
 @extends('template.main')
 @section('content-public')
+    {{-- hero kelas reguler --}}
     <section id="kelas-reguler" class="py-3 py-lg-5">
         <div class="container">
             <div class="row d-flex align-items-center justify-content-between">
@@ -22,4 +23,66 @@
             </div>
         </div>
     </section>
+    {{-- akhir hero kelas reguler --}}
+
+    {{-- bidang pelatihan --}}
+    <section id="bidang-pelatihan" class="py-3 py-lg-5">
+        <div class="container">
+            <div class="text-center mb-5">
+                <h2>Paket Bidang Pelatihan</h2>
+            </div>
+
+            <div class="d-flex flex-wrap flex-column flex-md-row justify-content-around gap-5">
+                <div class="col-12 col-md-5 paket bg-white p-2 rounded">
+                    <h3 class="fw-medium">Teknik Komputer (Hardware)</h3>
+                    <p class="fw-medium">Biaya Pendidikan Rp. 950.000</p>
+                    <p class="fw-medium">Durasi 2 Jam / Pertemuan</p>
+                    <p class="fw-medium">20 Pertemuan</p>
+                    <p class="fw-medium">Waktu Sesuai Kesepakatan Dengan Instruktur</p>
+                    <a href="" class="btn fw-medium">Ajukan Pertanyaan / Daftar</a>
+                </div>
+                <div class="col-12 col-md-5 paket bg-white p-2 rounded">
+                    <h3 class="fw-medium">Teknik Komputer (Hardware)</h3>
+                    <p class="fw-medium">Biaya Pendidikan Rp. 950.000</p>
+                    <p class="fw-medium">Durasi 2 Jam / Pertemuan</p>
+                    <p class="fw-medium">20 Pertemuan</p>
+                    <p class="fw-medium">Waktu Sesuai Kesepakatan Dengan Instruktur</p>
+                    <a href="" class="btn fw-medium">Ajukan Pertanyaan / Daftar</a>
+                </div>
+                <div class="col-12 col-md-5 paket bg-white p-2 rounded">
+                    <h3 class="fw-medium">Teknik Komputer (Hardware)</h3>
+                    <p class="fw-medium">Biaya Pendidikan Rp. 950.000</p>
+                    <p class="fw-medium">Durasi 2 Jam / Pertemuan</p>
+                    <p class="fw-medium">20 Pertemuan</p>
+                    <p class="fw-medium">Waktu Sesuai Kesepakatan Dengan Instruktur</p>
+                    <a href="" class="btn fw-medium">Ajukan Pertanyaan / Daftar</a>
+                </div>
+                <div class="col-12 col-md-5 paket bg-white p-2 rounded">
+                    <h3 class="fw-medium">Teknik Komputer (Hardware)</h3>
+                    <p class="fw-medium">Biaya Pendidikan Rp. 950.000</p>
+                    <p class="fw-medium">Durasi 2 Jam / Pertemuan</p>
+                    <p class="fw-medium">20 Pertemuan</p>
+                    <p class="fw-medium">Waktu Sesuai Kesepakatan Dengan Instruktur</p>
+                    <a href="" class="btn fw-medium">Ajukan Pertanyaan / Daftar</a>
+                </div>
+                <div class="col-12 col-md-5 paket bg-white p-2 rounded">
+                    <h3 class="fw-medium">Teknik Komputer (Hardware)</h3>
+                    <p class="fw-medium">Biaya Pendidikan Rp. 950.000</p>
+                    <p class="fw-medium">Durasi 2 Jam / Pertemuan</p>
+                    <p class="fw-medium">20 Pertemuan</p>
+                    <p class="fw-medium">Waktu Sesuai Kesepakatan Dengan Instruktur</p>
+                    <a href="" class="btn fw-medium">Ajukan Pertanyaan / Daftar</a>
+                </div>
+                <div class="col-12 col-md-5 paket bg-white p-2 rounded">
+                    <h3 class="fw-medium">Teknik Komputer (Hardware)</h3>
+                    <p class="fw-medium">Biaya Pendidikan Rp. 950.000</p>
+                    <p class="fw-medium">Durasi 2 Jam / Pertemuan</p>
+                    <p class="fw-medium">20 Pertemuan</p>
+                    <p class="fw-medium">Waktu Sesuai Kesepakatan Dengan Instruktur</p>
+                    <a href="" class="btn fw-medium">Ajukan Pertanyaan / Daftar</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- akhir bidang pelatihan --}}
 @endsection
